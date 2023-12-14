76442bc9ee0902b2145a7bb7eb50301b9ea08191
Ameliael <152161190+Ameliael@users.noreply.github.com>
2023-12-14 14:57:21 +0700

    Add files via upload

---
+++ Indeks.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+         <div class="row">
+            <div class="col-12">
+                <h3>Belajar Grafik</h3>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col 6"></div>
+            <div>
+             <canvas id="myChart"></canvas>
+            </div>
+            <div class="col 6"></div>
+         </div>
+    </div>
+    
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
+</body>
+</html>
+
+
+
+<script>
